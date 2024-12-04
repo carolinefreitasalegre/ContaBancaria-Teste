44fd94a2ea4c1b8285e7dac5b3dbc73ca1bd8143
Caroline Freitase <freitascaroline.92@gmail.com>
2024-12-04 09:54:27 -0400

    Adicionar arquivos de projeto.

---
+++ ContaBancaria/Dados.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContaBancaria
+{
+    internal class Dados
+    {
+        public int NumeroConta;
+        public string NomeTitular;
+        public double Saldo;
+
+        public Dados(int numeroConta, string nomeTitular, double depositoInicial = 0)
+        {
+            NumeroConta = numeroConta;
+            NomeTitular = nomeTitular;
+            Saldo = depositoInicial;
+
+        }
+
+
+        public void ExibirDados()
+        {
+            Console.WriteLine($"Numero da conta: {NumeroConta}");
+            Console.WriteLine($"Nome do titular: {NomeTitular}");
+            Console.WriteLine($"Saldo: {Saldo:F2}");
+        }
+
+        public void RealizarDeposito(double valor)
+        {
+            Saldo += valor;
+            Console.WriteLine($"Depósito no valor de R${valor} irealizado com sucesso.");
+        }
+
+        public void RealizarSaque(double valor)
+        {
+            const double taxa = 5.00;
+            Saldo -= ( valor + taxa);
+            Console.WriteLine($"Saque no valor de R${valor} realizado com sucesso. Taxa no valoe de R${taxa} foi cobrada!");
+        }
+    }
+}
---
+++ ContaBancaria/Program.cs
@@ -0,0 +1,39 @@
+﻿using ContaBancaria;
+
+Dados dados;
+
+Console.Write("Entre com o numero da conta: ");
+int numeroConta = int.Parse(Console.ReadLine());
+
+Console.Write("Entre com o titular da conta: ");
+string nomeTitular = Console.ReadLine().ToUpper();
+
+Console.Write("Haverá depósito inical (S/N)?");
+char opcao = char.ToUpper(Console.ReadKey().KeyChar);
+Console.WriteLine();
+
+double depositoInicial = 0;
+if(opcao == 'S')
+{
+    Console.WriteLine("Informe o valor do depósito inicial: ");
+    depositoInicial = double.Parse(Console.ReadLine());
+} else
+{
+    dados = new Dados(numeroConta,nomeTitular);
+}
+
+dados = new Dados(numeroConta, nomeTitular, depositoInicial);
+Console.WriteLine("Conta cadastrada com sucesso!");
+dados.ExibirDados();
+
+Console.WriteLine("Informe o valor do depósito: ");
+double valorDeposito = double.Parse(Console.ReadLine());
+dados.RealizarDeposito(valorDeposito);
+dados.ExibirDados();
+
+Console.WriteLine("Informe mo valor para saque: ");
+double valorSaque = double.Parse(Console.ReadLine());
+dados.RealizarSaque(valorSaque);
+dados.ExibirDados();
+
+Console.WriteLine("Obrigado por utilizar nosso sistema.");
